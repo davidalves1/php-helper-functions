14427b383b967f3be22fe24f5b70e58d542e9fdc
David Alves de Souza <alves.david@outlook.com>
2017-02-16 00:03:21 -0200

    Update helper.php

--- helper.php
+++ helper.php
@@ -21,3 +21,67 @@ function array_keys_change($array, $keys_change)
 
 	return array_combine($keys, $values);
 }
+
+/**
+ * Change the date to a new format
+ * @param  string $date
+ * @param  string $original_format
+ * @param  string $new_format
+ * @return string 
+ */
+function format_date($date, $new_format, $original_format = 'dd/mm/yyyy')
+{
+	switch (strtolower($original_format)) {
+		case 'dd/mm/yyyy':
+		case 'dd-mm-yyyy':
+			list($day, $month, $year) = preg_split('/[-.\/ ]/', $date);
+			break;
+
+	 	case 'yyyy/dd/mm':
+		case 'yyyy-dd-mm':
+			list($year, $day, $month) = preg_split("/[-.\/ ]/", $date);
+			break;
+		
+		case 'mm-dd-yyyy':
+		case 'mm/dd/yyyy':
+			list($month, $day, $year) = preg_split("/[-.\/ ]/", $date);
+			break;
+
+	 	case 'yyyy/mm/dd':
+		case 'yyyy-mm-dd':
+			list($year, $day, $month) = preg_split("/[-.\/ ]/", $date);
+			break;
+
+		case 'yyyymmdd':
+			$year = substr($date, 0, 4);
+			$month = substr($date, 4, 2);
+			$day = substr($date, 6, 2);
+			break;
+
+		case 'yyyyddmm':
+			$year = substr($date, 0, 4);
+			$day = substr($date, 4, 2);
+			$month = substr($date, 6, 2);
+			break;
+
+		default:
+			return false;
+	}
+
+	$accepted_formats = [
+		'd/m/Y',
+		'm/d/Y',
+		'Y/d/m',
+		'd-m-Y',
+		'm-d-Y',
+		'Y-m-d',
+		'dmY',
+		'mdY',
+		'Ymd'
+	];
+		
+	if (!checkdate($month, $day, $year) || !in_array($new_format, $accepted_formats))
+			return false;
+
+	return date($new_format, strtotime(preg_replace("/\D/", '-', $date)));
+}
